b7925d8fbf6041859332a873f59fa7584137671f
manchenkoff <artem@manchenkoff.me>
2024-04-20 20:20:39 +0200

    added nuxt-auth-sanctum module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,16 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui", "@nuxt/eslint"],
+
+  modules: ["@nuxt/ui", "@nuxt/eslint", "nuxt-auth-sanctum"],
+
   eslint: {
     config: {
       stylistic: true,
     },
+  },
+
+  sanctum: {
+    baseUrl: "http://localhost:80",
   },
 });
