bfa67d7220f0be3604c2499b7441df6e2d35940e
manchenkoff <artem@manchenkoff.me>
2024-04-20 20:12:21 +0200

    added eslint with formatter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"],
+  modules: ["@nuxt/ui", "@nuxt/eslint"],
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
 });
