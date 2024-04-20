c75900069a95fcf7d9c62dde9d0b5705f558b250
manchenkoff <artem@manchenkoff.me>
2024-04-20 19:42:20 +0200

    migrated to the latest Yarn

--- app.vue
+++ app.vue
@@ -4,10 +4,21 @@
       <template #header>
         <div class="flex justify-between">
           <h1>Welcome to Nuxt UI Starter</h1>
-          <ColorScheme><USelect v-model="$colorMode.preference" :options="['system', 'light', 'dark']" /></ColorScheme>
+          <ColorScheme>
+            <USelect
+              v-model="$colorMode.preference"
+              :options="['system', 'light', 'dark']"
+            />
+          </ColorScheme>
         </div>
       </template>
-      <UButton icon="i-heroicons-book-open" to="https://ui.nuxt.com" target="_blank">Open Nuxt UI Documentation</UButton>
+      <UButton
+        icon="i-heroicons-book-open"
+        to="https://ui.nuxt.com"
+        target="_blank"
+      >
+        Open Nuxt UI Documentation
+      </UButton>
     </UCard>
   </UContainer>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,5 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  modules: ["@nuxt/ui"]
-})
+  modules: ["@nuxt/ui"],
+});
