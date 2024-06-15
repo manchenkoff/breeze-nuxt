0531d4ed06d267b02473490af3d29249cede539b
manchenkoff <artem@manchenkoff.me>
2024-06-16 01:22:28 +0200

    fix: prevent nullable error after logging out

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,16 +1,18 @@
 <script lang="ts" setup>
-
+const { isAuthenticated } = useSanctumAuth()
 </script>
 
 <template>
-  <div class="flex py-4 px-5 justify-between items-center">
-    <NavBar />
-    <ProfileMenu />
-  </div>
+  <template v-if="isAuthenticated">
+    <div class="flex py-4 px-5 justify-between items-center">
+      <NavBar />
+      <ProfileMenu />
+    </div>
 
-  <UContainer class="mt-6 flex flex-col gap-4">
-    <slot />
-  </UContainer>
+    <UContainer class="mt-6 flex flex-col gap-4">
+      <slot />
+    </UContainer>
+  </template>
 </template>
 
 <style scoped></style>
